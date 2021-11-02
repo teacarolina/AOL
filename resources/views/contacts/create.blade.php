6d97946d79fce7ae897eaa40b40e16ced5121c6a
teacarolina <teacarolina@icloud.com>
2021-11-02 14:55:57 +0100

    Adding design

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 <div class="row">
     <div class="col-sm">
-        <h1 style="font-family: 'Georgia', sans-serif; color: #333333;" class="display-10 text-center">Welcome to Wedsly</h1>
+        <h1 style="font-family: 'Georgia', sans-serif; color: #333333;" class="display-10 text-center pt-4">Welcome to Wedsly</h1>
         <!--<h3 class="display-10">Already a member?</h3>-->
         <h6 style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #333333;" class="display-10 text-center">Already a member? <b>Log in</b> or <b>Merge accounts</b></h6>
         <div>
@@ -19,7 +19,7 @@
             @endif
             <form method="post" action="{{ route('contacts.store') }}">
                 @csrf
-                <div class="form-group">
+                <div class="form-group pt-4">
                     <label style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #333333; font-size: 10px;" for="email">E-MAIL*</label>
                     <input type="text" class="form-control" name="email"/>
                 </div>
@@ -37,16 +37,16 @@
                 </div>
                 </div>
                 </div>
-                <div class="form-group form-check-inline">
+                <div class="form-group form-check-inline pt-4">
                 <input type="checkbox" class="form-check-input" name="terms" value="yes" required/>
                 <label style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #333333; font-size: 12px;" for="terms" class="form-check-label">I accept that Wedsly will store and process my information</label>
                 </div>
-                <div class="form-group">
+                <div class="form-group pt-4">
                 <button style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #fbf8f8;" type="submit" class="btn btn-secondary btn-lg btn-block">CREATE FREE ACCOUNT</button>
                 </div>
             </form>
-            <h6 style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #333333;" class="display-10 text-center">OR SIGN IN WITH</h6>
-            <div class="row">
+            <h6 style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #333333;" class="display-10 text-center pt-2">OR SIGN IN WITH</h6>
+            <div class="row pt-2">
                 <div class="col">
                 <div class="form-group">
              <button type="submit" class="btn btn-outline-secondary btn-lg btn-block">
@@ -65,24 +65,36 @@
             </div>
         </div>
         <div class="col-sm">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide h-75" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+   <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
   </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
+  <div class="carousel-inner h-100">
+    <!--<div class="carousel-item active">
       <img class="d-block w-100" src="/images/bridal-bouquet.jpg" alt="First slide">
-     <!--  <div class="carousel-caption d-none d-md-block">
+       <div class="carousel-caption d-none d-md-block">
     <h1>A few clicks away from planning your dream wedding!</h1>
-  </div> -->
+  </div> 
+    </div>-->
+    <div class="carousel-item active">
+    <div class="carousel-caption d-none d-md-block h-100 text-right">
+    <img style="width: 70px; height:70px;" src="/images/Com@1x.svg" alt="wedsly icon" class="icon">
+    </div>
+      <img class="d-block w-100" src="/images/couple.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block h-50">
+    <h3 style="font-family: 'Georgia', sans-serif; color: #333333;" class="text-left">All roles in one account!<br/> Welcome to Wedsly 3.0</h3>
+    <h6 style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #333333;" class="text-left">Switch between roles
+    easily from any Wedsly dashboard. One login, one password, all possibilities.</h6>
+    <button style="font-family: 'Arial Regular', 'Arial', sans-serif; color: #fbf8f8;" type="submit" class="btn btn-dark btn-lg btn-block">LOG IN AND MERGE</button>
+  </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="/images/couple.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="/images/sunset.jpg" alt="Third slide">
+        <img class="d-block w-100" src="/images/sunset.jpg" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block h-50">
+    <h1 style="font-family: 'Georgia', sans-serif; color: #fbf8f8;" class="text-left">A few clicks away from planning your dream wedding!</h1>
+  </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
